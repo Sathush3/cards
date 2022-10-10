d84cde516c8b36c9b8bd6dd5637990e1f8eec395
Sathush Chandran <sathush3@gmail.com>
2022-10-10 11:53:43 +0530

    Initial Commit

---
+++ cards/cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardsApp.swift
+//  cards
+//
+//  Created by Sathurshan Ramachandran on 2022-10-10.
+//
+
+import SwiftUI
+
+@main
+struct cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
